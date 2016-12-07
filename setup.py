8eb8f8f5887ad44dff51e9aaaa9cb8820588345d
Christian Roth <christian.roth@mpibpc.mpg.de>
2016-12-07 16:25:02 +0100

    added auxiliary scripts in source distribution

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     version='1.1.0',
     packages=find_packages(),
     install_requires=install_deps,
+    package_data={
+        'pipeclip.lib': ['*.R', '*.sh']
+    },
     entry_points={
         'console_scripts': [
             'pipeclip = pipeclip.pipeclip:main',
